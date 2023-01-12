017e40535d02cf4cce283b89da813ebe88291088
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-12 00:31:50 -0300

    add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,8 @@
-export const App = () => <h1>Hello World</h1>
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+export const App = () => (
+  <ThemeProvider theme={defaultTheme}>
+    <h1>Hello World</h1>
+  </ThemeProvider>
+)
