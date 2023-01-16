9dfce05d45aa1b953267392c468e2ba4f8c70633
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-01-16 15:20:42 -0300

    add config to validate form

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,40 @@
 import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { SearchFormContainer } from './styles'
 
-export const SearchForm = () => (
-  <SearchFormContainer>
-    <input type="text" placeholder="Busque por transações" />
+const searchFormSchema = z.object({ query: z.string() })
 
-    <button type="submit">
-      <MagnifyingGlass size={20} />
-      Buscar
-    </button>
-  </SearchFormContainer>
-)
+type SearchFormImputs = z.infer<typeof searchFormSchema>
+
+export const SearchForm = () => {
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+    register,
+    reset,
+  } = useForm<SearchFormImputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  const handleSearchTransactions = async (data: SearchFormImputs) => {
+    console.log(data)
+    reset()
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
+    </SearchFormContainer>
+  )
+}
